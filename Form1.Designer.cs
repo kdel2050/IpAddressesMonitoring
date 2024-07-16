9644a8681e45d4303234792592af914ef804ffa1
kdel2050 <163053456+kdel2050@users.noreply.github.com>
2024-07-16 11:00:32 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace NetMonitor
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.cmdSelectFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmdSelectOutDir = new System.Windows.Forms.Button();
+            this.cmdExit = new System.Windows.Forms.Button();
+            this.cmdStartChecking = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // cmdSelectFile
+            // 
+            this.cmdSelectFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.cmdSelectFile.Location = new System.Drawing.Point(28, 91);
+            this.cmdSelectFile.Name = "cmdSelectFile";
+            this.cmdSelectFile.Size = new System.Drawing.Size(406, 45);
+            this.cmdSelectFile.TabIndex = 0;
+            this.cmdSelectFile.Text = "Επιλέξτε &Αρχείο IP Διευθύνσεων - Απλό Text ....";
+            this.cmdSelectFile.UseVisualStyleBackColor = true;
+            this.cmdSelectFile.Click += new System.EventHandler(this.cmdSelectFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(24, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(550, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Σύστημα Ελέγχου Λειτουργικότητας Διακομιστών με Διεύθυνση IP:";
+            // 
+            // cmdSelectOutDir
+            // 
+            this.cmdSelectOutDir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.cmdSelectOutDir.Location = new System.Drawing.Point(28, 180);
+            this.cmdSelectOutDir.Name = "cmdSelectOutDir";
+            this.cmdSelectOutDir.Size = new System.Drawing.Size(406, 45);
+            this.cmdSelectOutDir.TabIndex = 2;
+            this.cmdSelectOutDir.Text = "Επιλέξτε Φάκελο Εγγραφής των Αποτελεσμάτων....";
+            this.cmdSelectOutDir.UseVisualStyleBackColor = true;
+            this.cmdSelectOutDir.Click += new System.EventHandler(this.cmdSelectOutDir_Click);
+            // 
+            // cmdExit
+            // 
+            this.cmdExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.cmdExit.ForeColor = System.Drawing.Color.Red;
+            this.cmdExit.Location = new System.Drawing.Point(419, 361);
+            this.cmdExit.Name = "cmdExit";
+            this.cmdExit.Size = new System.Drawing.Size(154, 45);
+            this.cmdExit.TabIndex = 3;
+            this.cmdExit.Text = "Έ&ξοδος";
+            this.cmdExit.UseVisualStyleBackColor = true;
+            this.cmdExit.Click += new System.EventHandler(this.cmdExit_Click);
+            // 
+            // cmdStartChecking
+            // 
+            this.cmdStartChecking.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.cmdStartChecking.ForeColor = System.Drawing.Color.Green;
+            this.cmdStartChecking.Location = new System.Drawing.Point(28, 271);
+            this.cmdStartChecking.Name = "cmdStartChecking";
+            this.cmdStartChecking.Size = new System.Drawing.Size(406, 47);
+            this.cmdStartChecking.TabIndex = 4;
+            this.cmdStartChecking.Text = "Έναρξη Ελέγχου Λειτουργικότητας Διακομιστών";
+            this.cmdStartChecking.UseVisualStyleBackColor = true;
+            this.cmdStartChecking.Click += new System.EventHandler(this.cmdStartChecking_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 426);
+            this.Controls.Add(this.cmdStartChecking);
+            this.Controls.Add(this.cmdExit);
+            this.Controls.Add(this.cmdSelectOutDir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdSelectFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Επιτηρητής Δικτύων";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdSelectFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cmdSelectOutDir;
+        private System.Windows.Forms.Button cmdExit;
+        private System.Windows.Forms.Button cmdStartChecking;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO;
+using System.Threading;
+using System.Security.Cryptography;
+
+namespace NetMonitor
+{
+    public partial class frmMain : Form
+    {
+        static string IPaddrFileNames = "";
+        static string ResultsFolder = "";
+        static bool checkingProcedure = true;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdSelectFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // ΕΠΙΛΟΓΗ ΑΡΧΕΙΟΥ ΑΠΛΟΥ ΚΕΙΜΕΝΟΥ ΠΟΥ ΣΕ ΚΑΘΕ ΓΡΑΜΜΗ ΠΕΡΙΕΧΕΙ ΜΙΑ IP ΔΙΕΥΘΥΝΣΗ
+                // ΔΗΛΑΔΗ ΠΧ 150 IP ΔΙΕΥΘΥΝΣΕΙΣ ΤΙΣ ΟΠΟΙΕΣ ΠΡΕΠΕΙ ΕΜΕΙΣ ΝΑ ΕΠΙΤΗΡΟΥΜΕ ΝΑ ΕΙΝΑΙ ΔΙΑΘΕΣΙΜΕΣ
+                // Η ΚΑΘΕ ΜΙΑ ΣΕ ΚΑΘΕ ΓΡΑΜΜΗ ΑΡΑ ΑΠΛΟ ΑΡΧΕΙΟ ΚΕΙΜΕΝΟΥ 150 ΓΡΑΜΜΩΝ
+                //openFileDialog1.RestoreDirectory = true;
+                openFileDialog1.Title = "Επιλογή Αρχείου με IP Διευθύνσεις...";
+                openFileDialog1.DefaultExt = "txt";
+                openFileDialog1.Filter = "Αρχεία Κειμένου (*.txt)|*.txt|Όλα τα αρχεία (*.*)|*.*";
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    IPaddrFileNames = openFileDialog1.FileName;
+                }
+
+                //MessageBox.Show(IPaddrFileNames);
+
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
+            }
+        }
+
+        private void cmdSelectOutDir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //ΕΠΙΛΟΓΗ ΜΟΝΟ ΤΟΥ ΦΑΚΕΛΟΥ ΟΠΟΥ ΓΡΑΦΟΝΤΑΙ ΤΑ LOG FILES ΜΕ ΤΑ ΑΠΟΤΕΛΕΣΜΑΤΑ ΤΗΣ PING IPCONFIG ΚΛΠ
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    ResultsFolder = folderBrowserDialog1.SelectedPath;
+                }
+
+                //MessageBox.Show(ResultsFolder);
+
+
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
+            }
+        }
+
+        private void cmdStartChecking_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                backgroundWorker1.RunWorkerAsync();
+
+                // ΠΡΩΤΟ ΒΗΜΑ ΑΠΟ ΤΟ ΑΠΛΟ ΑΡΧΕΙΟ ΚΕΙΜΕΝΟΥ ΒΑΖΕΙ ΜΙΑ ΜΙΑ ΤΙΣ ΔΙΕΥΘΥΝΣΕΙΣ ΣΕ ΠΙΝΑΚΑ STRINGLIST
+                // ΓΙΑ ΚΑΘΕ ΣΤΟΙΧΕΙΟ ΠΙΝΑΚΑ ΔΗΛΑΔΗ ΚΑΘΕ IP ADDRESS ΕΚΤΕΛΕΣΗ ΕΝΤΟΛΗΣ PING ΚΑΙ ΤΟ ΑΠΟΤΕΛΕΣΜΑ ΤΗΣ ΝΑ ΓΡΑΦΕΤΑΙ ΣΤΟ LOG FILE
+                // ΔΗΛΑΔΗ MILLISECOND ΧΡΟΝΟΥ ΚΛΠ Η ΜΗ ΑΠΟΚΡΙΣΗ ΤΗΣ ΔΙΕΥΘΥΝΣΗΣ - ΤΕΛΟΣ ΑΝ ΚΑΠΟΙΑ ΔΙΕΥΘΥΝΣΗ ΕΙΝΑΙ OFF ΜΠΟΡΕΙ ΝΑ ΣΤΕΛΝΕΙ 
+                // ΚΑΙ EMAIL ΕΝΗΜΕΡΩΤΙΚΟ ΣΕ ΜΙΑ ΔΙΕΥΘΥΝΣΗ ΔΙΑΧΕΙΡΙΣΤΗ
+                // ΣΤΗ ΓΡΑΜΜΗ ΤΟΥ LOG FILE ΚΑΤΑΓΡΑΦΕΤΑΙ ΤΟ ΑΠΟΤΕΛΕΣΜΑ ΤΗΣ PING, IP ADRESS ΚΑΙ ΧΡΟΝΟΣΗΜΑΝΣΗ ΣΥΣΤΗΜΑΤΟΣ
+                // ΥΠΑΡΧΕΙ ΧΡΟΝΟΚΑΘΥΣΤΕΡΗΣΗ (SLEEP - PAUSE) ΣΤΟ ΝΗΜΑ ΓΙΑ ΝΑ ΜΗΝ ΚΑΝΟΥΝ BAN ΤΗΝ ΕΦΑΡΜΟΓΗ ΤΑ ΕΠΙΜΕΡΟΥΣ FIREWALLS ΚΛΠ
+                // ΤΟ LOG FILE ΠΑΙΡΝΕΙ ΟΝΟΜΑ ΧΡΟΝΟΣΗΜΑΝΣΗ ΚΑΙ ΤΗΝ ΚΑΤΑΛΗΞΗ ΠΧ TXT Η LOG ΔΗΛΑΔΗ ΠΧ 19-06-2024-11-30-35.TXT
+
+                // ΑΦΟΥ ΕΧΕΙ ΓΡΑΦΤΕΙ ΤΟ ΠΡΩΤΟ LOG FILE ANAMENEI SLEEP ΤΟ ΛΟΓΙΣΜΙΚΟ ΠΧ 10 ΔΕΥΤΕΡΟΛΕΠΤΑ ΙΣΩΣ ΕΩΣ ΚΑΙ 10 ΛΕΠΤΑ
+                // ΚΑΙ ΞΑΝΑΕΠΑΝΑΛΑΜΒΑΝΕΙ ΤΗ ΔΙΑΔΙΚΑΣΙΑ ΣΕ ΕΠΟΜΕΝΟ LOG FILE ΜΕ ΤΗΝ ΑΝΤΙΣΤΟΙΧΗ ΝΕΑ ΧΡΟΝΟΣΗΜΑΝΣΗ.
+
+                // ΦΥΣΙΚΑ ΤΟ ΛΟΓΙΣΜΙΚΟ ΕΠΙΤΗΡΕΙ ΑΔΙΑΛΕΙΠΤΑ ΚΑΙ ΚΑΤΑΓΡΑΦΕΙ ΜΕ ΤΑ LOGS ΕΚΤΟΣ ΑΝ ΠΑΤΗΣΕΙ Ο ΧΡΗΣΤΗΣ ΕΞΟΔΟ
+                // ΚΑΙ ΣΕ ΚΑΘΕ ΕΠΑΝΑΛΗΠΤΙΚΗ ΔΙΑΔΙΚΑΣΙΑ ΣΤΕΛΝΕΙ ΣΤΟΝ ΔΙΑΧΕΙΡΙΣΤΗ ΠΟΙΕΣ IP BRHKE DOWN ΚΑΙ ΤΗ ΧΡΟΝΙΚΗ ΣΤΙΓΜΗ. 
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
+            }
+
+        }
+
+        private int RandomSeconds()
+        // δημιουργει εναν ψευδοτυχαιο ακεραιο απο 100 εως και 9999
+        {
+            try { 
+
+                var random = new Random();
+                int res = random.Next(100, 10000); // αριθμός από 100 εως και 10000 millisecond
+                return res;
+            }
+            catch (Exception ex)
+            { 
+                Debug.Print(ex.Message);
+                return 1000;
+            }
+
+
+        }
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while ( checkingProcedure == true)
+            // αν δε πατησει exit ο χρηστης το συστημα καταγραφει συνεχως
+            { 
+                DataTable pingResults = new DataTable();
+
+
+                pingResults.Clear();
+
+                var logFile = File.ReadAllLines(IPaddrFileNames);
+                // διαβασε τις IP διευθυνσεις
+
+                var ipAddress = new List<string>(logFile);
+
+                Parallel.For(0, ipAddress.Count(), (i, loopState) =>
+                // κανε ασυγχρονο ελεγχο με την εντολη ping και καταγραψε τα αποτελεσματα
+                {
+                    Ping ping = new Ping();
+                    PingReply pingReply = ping.Send(ipAddress[i].ToString());
+
+                    this.BeginInvoke((Action)delegate ()
+                    {
+                    
+                        if (pingReply.Status == IPStatus.Success)
+                        {
+                            // Εγγραφή στο Log File οτι η IP ειναι ΟΚ
+                        }
+                        else
+                        { 
+                            // Εγγραφή στο Log File οτι η IP δεν είναι ΟΚ και email σε διαχειριστή
+                        }
+
+                    });
+
+                    Thread.Sleep(RandomSeconds());
+                    // ΕΔΩ ΘΕΛΕΙ ΠΕΙΡΑΜΑΤΙΣΜΟ ΤΑ ΔΕΥΤΕΡΟΛΕΠΤΑ ΤΗΣ ΩΡΑΣ ΓΙΑ ΝΑ ΜΗΝ ΚΑΝΟΥΝ BAN ΤΑ FIREWALL ΤΟ APP
+
+                    Application.DoEvents();
+
+                });
+                
+            }
+
+
+        }
+
+
+        private void cmdExit_Click(object sender, EventArgs e)
+        {
+            // ΕΞΟΔΟΣ ΑΠΟ ΤΗΝ ΕΠΑΝΑΛΗΠΤΙΚΗ ΔΙΑΔΙΚΑΣΙΑ ΕΛΕΓΧΟΥ ΚΑΙ ΚΛΕΙΣΙΜΟ ΤΗΣ ΕΦΑΡΜΟΓΗΣ.
+
+            checkingProcedure = false;
+
+            Application.Exit();
+        }
+    }
+}
